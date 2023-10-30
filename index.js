87994579f282e1343fc6f5647f3e92555eb589fb
Faruks23 <mdfaruksp2@gmail.com>
2023-10-30 14:55:05 +0600

    make service api, project api, review api

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const cors = require("cors");
 require("dotenv").config();
 app.use(cors())
 app.use(express.json());
+
+
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -13,7 +16,7 @@ app.get("/", (req, res) => {
 
 //  mongodb  connection
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_Name}:${process.env.DB_KEY}@cluster0.1fsmjmg.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,27 +35,212 @@ async function run() {
 
     // collection
     const userCollection = client.db("Alamin").collection("users");
-     const servicesCollection = client.db("Alamin").collection("services");
+    const SocialCollection = client.db("Alamin").collection("Social");
+    const servicesCollection = client.db("Alamin").collection("services");
     // save user information
 
-    app.post("/users", async (req, res) => {
+    // save user information
+    app.post("/AddUsers", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
+
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // Get user collection
+    app.get("/GetUsers", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    // make admin a normal user
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+          disabled: true,
+        },
+      };
+
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    // get social media information
+    app.get("/social", async (req, res) => {
+      const result = SocialCollection.find().toArray();
+      res.send(result);
+    });
+    // add social media information
+
+    app.post("/Social/post", async (req, res) => {
       const data = req.body;
-      const result = await userCollection.insertOne(data);
+      const result = await SocialCollection.insertOne(data);
+      res.send(result);
+    });
+    // update social information
+    app.put("/Social/update", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          name: data.name,
+          Link: data.link,
+        },
+      };
+      const result = await SocialCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    // delete social information
+    app.delete("/Social/delete", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = SocialCollection.deleteOne(filter);
+      res.send(result);
+    });
+
+    // get Project information
+    const ProjectCollection = client.db("Alamin").collection("Project");
+
+    app.get("/Project", async (req, res) => {
+      const result = await ProjectCollection.find().toArray();
+      res.send(result);
+    });
+
+    // add new project
+
+    app.post("/Project/add", async (req, res) => {
+      const data = req.body;
+      const result = await ProjectCollection.insertOne(data);
+      res.send(result);
+    });
+
+    // update Project information
+
+    app.put("/Project/update", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          name: data.name,
+          image: data.image,
+          description: data.description,
+        },
+      };
+
+      const result = await ProjectCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    // delete Project information
+
+    app.delete("/Project/delete", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = ProjectCollection.deleteOne(filter);
       res.send(result);
     });
 
     // get services information
     app.get("/services", async (req, res) => {
-        
       const result = await servicesCollection.find().toArray();
-       res.send(result);
-       
+      res.send(result);
     });
 
+    // add services information
 
+    app.post("/service/add", async (req, res) => {
+      const data = req.body;
+      const result = await servicesCollection.insertOne(data);
+      res.send(result);
+    });
 
+    // update services information
 
+    app.put("/service/update", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          name: data.name,
+          image: data.image,
+          description: data.description,
+        },
+      };
 
+      const result = await servicesCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    // delete services information
+
+    app.delete("/service/delete", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = servicesCollection.deleteOne(filter);
+      res.send(result);
+    });
+
+    
+    // review services information
+    // review services information
+
+    // get services information
+    app.get("/services", async (req, res) => {
+      const result = await servicesCollection.find().toArray();
+      res.send(result);
+    });
+
+    // add services information
+
+    app.post("/service/add", async (req, res) => {
+      const data = req.body;
+      const result = await servicesCollection.insertOne(data);
+      res.send(result);
+    });
+
+    // update services information
+
+    app.put("/service/update", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          name: data.name,
+          image: data.image,
+          description: data.description,
+        },
+      };
+
+      const result = await servicesCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    // delete services information
+
+    app.delete("/service/delete", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = servicesCollection.deleteOne(filter);
+      res.send(result);
+    });
+
+    // review services information
+    // review services information
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
